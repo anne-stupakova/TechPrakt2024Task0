34e79c7d719b6694196183c5abeb24f7162597df
Ann Stupakova <ipz223_sas@student.ztu.edu.ua>
2024-02-06 21:25:02 +0200

    add gitignore and project

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = UTF8Encoding.UTF8;
+
+        Console.WriteLine("Введіть перше число:");
+        string input1 = Console.ReadLine();
+
+        Console.WriteLine("Введіть друге число:");
+        string input2 = Console.ReadLine();
+
+        if (double.TryParse(input1, out double number1) && double.TryParse(input2, out double number2))
+        {
+            double sum = number1 + number2;
+            Console.WriteLine($"Сума чисел {number1} та {number2} дорівнює: {sum}");
+        }
+        else
+        {
+            Console.WriteLine("Некоректний ввід. Будь ласка, введіть числа.");
+        }
+
+        Console.ReadLine();
+    }
+}
